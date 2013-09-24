1e77538e91de3ac22ad32e97582d5a8e45136a9c
cmj0121 <cmj0121@gmail.com>
2013-09-24 09:31:58 +0800

    [Feature] Add clear all .pyc on __main__.py
    	Clear all non-necessary files in project

--- __main__.py
+++ __main__.py
@@ -2,7 +2,16 @@
 #! coding: utf-8
 
 import interact
+import sys
 
+def clear():
+	import os
 
+	ret = [_ for _ in os.popen("find . | grep pyc").read().split('\n') if _]
+	for _ in ret:
+		os.unlink(_)
 if __name__ == "__main__":
+	if 1 < len(sys.argv) and "clear" == sys.argv[1]:
+		clear()
+		exit(0)
 	interact.interact()
