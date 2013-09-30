58082dc54539fe982287861ca618e48fee6b9ccc
cmj0121 <cmj0121@gmail.com>
2013-09-30 19:04:25 +0800

    <Refine> Refine the task.py
    	1- Using only one public function on task
    	2- Refine the delete process method

--- sdk/task.py
+++ sdk/task.py
@@ -3,21 +3,55 @@
 
 from getpass import getpass
 from multiprocessing import Process
+import os
 import sys
 
 
 class _Task_(object):
 	def __init__(self, func):
-		self.func = func
+		if (isinstance(func, str) or isinstance(func, unicode)):
+			try:
+				self.func = eval(func)
+			except NameError as e:
+				raise SystemError("Function %s is not defined" %func)
+		elif callable(func):
+			self.func = func
+		else:
+			raise SystemError("We only support run function")
+
+		if not callable(self.func):
+			raise SystemError("We only support run function")
+	def __del__(self):
+		if -1 == self.pid: return
+
+		os.unlink(self.pidFile)
+		self.proc.terminate()
+		self.proc.join()
 	def __str__(self):
-		return "Process [%s] is %s" %(self.func.func_name, "alive" if self.proc.is_alive() else "dead")
-	def DEL(self):
-		if hasattr(self, 'proc') and hasattr(self.proc, "is_alive") and self.proc.is_alive():
-			self.proc.terminate()
+		if -1 == self.pid:
+			st = "dead"
+		else:
+			with open(self.pidFile) as f:
+				st = "alive (%s)" %f.read()
+		return "Process [%s] is %s" %(self.func.func_name, st)
+	def __repr__(self):
+		return str(self)
+
+	@property
+	def pid(self):
+		if hasattr(self, "proc") and self.proc.is_alive():
+			return self.proc.pid
+		else:
+			return -1
+	@property
+	def pidFile(self):
+		if -1 == self.pid:
+			raise SystemError("Process not run!")
+		return "/tmp/task.{pid}".format(pid=self.pid)
+
 	def _loadParm_(self, func_name, doc):
 		while True:
-			print "Do you want to run %s [y/N]:" %(func_name), 
-			ans = raw_input().upper()
+			ans = raw_input("Do you want to run %s [y/N]: " %(func_name)).upper()
 			if ans and ans[0] not in ('Y', 'N', ''):
 				continue
 
@@ -25,7 +59,10 @@ class _Task_(object):
 			break
 		if "N" == ans[0]: return
 
+		if not doc: return {}
+		elif not isinstance(doc, str) and not isinstance(doc, unicode): return {}
 		parm = {}
+
 		for n in (line for line in doc.split('\n') if line.strip().startswith('@')):
 			key = n.split(':')
 			key, value = key[0].strip()[1:], ":".join(key[1:]).strip()
@@ -42,44 +79,48 @@ class _Task_(object):
 
 		return parm
 	def run(self):
-		func = eval(self.func) if isinstance(self.func, str) else self.func
+		func = self.func
+
 		parm = self._loadParm_(func.func_name, func.func_doc)
-		if not parm: return
+		if not isinstance(parm, dict): return
 
 		self.proc = Process(target=func, kwargs=parm)
 		self.proc.start()
+		if not os.path.exists(self.pidFile):
+			with open(self.pidFile, "w") as f:
+				pass
 		return True
 class Task(object):
-	CURRENT_JOB = []
+	_jobs_ = []
 
 	def __str__(self):
+		return self.job
+	def __repr__(self):
+		return str(self)
+	def __del__(self):
+		del self._jobs_[:]
+	def __delitem__(self, index):
+		del self._jobs_[index]
+	def __getitem__(self, index):
+		return self._jobs_[index]
+	def _getJob_(self):
 		ret, cnt = [], 1
-		for n in self.CURRENT_JOB:
+		for n in self._jobs_:
 			ret += ["[%d] %s" %(cnt, str(n))]
 			cnt += 1
 		if ret: return "\n".join(ret)
 		else: return "No current job or task"
-	def __repr__(self):
-		return str(self)
-	def DEL(self):
-		for n in self.CURRENT_JOB:
-			n.DEL()
-	def __del__(self):
-		self.DEL()
-	def __delitem__(self, index):
-		if hasattr(self.CURRENT_JOB[index], "DEL") and callable(self.CURRENT_JOB[index].DEL):
-			self.CURRENT_JOB[index].DEL()
-		del self.CURRENT_JOB[index]
-	def __getitem__(self, index):
-		return self.CURRENT_JOB[index]
 	def _addJob_(self, func):
 		if not isinstance(func, list) and not isinstance(func, tuple):
-			func = (func,)
+			func = (func, )
 		for f in func:
 			t = _Task_(f)
 			if t.run():
-				self.CURRENT_JOB += [t]
+				self._jobs_ += [t]
 	def _deljob_(self, index=0):
-		del self.CURRENT_JOB[index]
-	addJob = property(__str__, _addJob_, None)
+		if isinstance(index, int):
+			del self._jobs_[index]
+		else:
+			raise KeyError("Not support key=%s" %key)
+	job = property(_getJob_, _addJob_, _deljob_)
 
